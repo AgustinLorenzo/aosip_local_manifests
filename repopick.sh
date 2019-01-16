202e8bf5b4dd00e7ae453a01e56feb8c60b843c6
AgustinLorenzo <agustin.lorenzo@thinco.es>
2019-01-16 22:16:25 +0000

    Updated the repopick

--- repopick.sh
+++ repopick.sh
@@ -2,6 +2,15 @@
 
 source build/envsetup.sh
 
+repopick 6788
+repo sync
 repopick -t button-lights
-repopick -t dual-sim
-repopick 6338 6622
+repopick 6338
+repopick -t blackaf
+repopick -t amb-viz
+repopick -t sim-stuff
+repopick -t sim-stuff-v2
+repopick -t net-traffic
+repopick -t translations
+repopick 6776 6789 6774
+
