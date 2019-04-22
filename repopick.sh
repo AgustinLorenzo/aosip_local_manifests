79cc23961d38e58b4a514962999345b70821b06b
AgustinLorenzo <agustin.lorenzo@thinco.es>
2019-04-23 00:53:29 +0200

    Updated the repopick

--- repopick.sh
+++ repopick.sh
@@ -2,10 +2,4 @@
 
 source build/envsetup.sh
 
-repopick -t changelog
-repopick -t l3
-repopick -t refactor-kernel-modules
-repopick -t bt-icon
-repopick -t wps
-repopick -t exposures
-repopick 7046 7519 7520 7523 7524
+repopick -t pa-pocket sb-dt2s
