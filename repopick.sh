4a5d5f3c4c4b2f30f4da72749e95c9a2b93a9953
AgustinLorenzo <agustin.lorenzo@thinco.es>
2020-04-07 23:58:30 +0200

    Updated the repopick

--- repopick.sh
+++ repopick.sh
@@ -2,4 +2,4 @@
 
 source build/envsetup.sh
 
-repopick -t fingerprint net-traf updater-notif-fixup-ten recovery-dyn camera av-merge overlay tinfoil-hat configs hide-gesture ae-stuffs && repopick 11792-11807
+repopick -t pixel fingerprint-zl1_zl0
