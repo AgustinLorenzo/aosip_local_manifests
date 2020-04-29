c2c70c7d76e391c6545397ac8af81645ea37ff81
AgustinLorenzo <agustin.lorenzo@thinco.es>
2020-04-29 06:48:58 +0200

    Updated the repopick with the latest build published

--- repopick.sh
+++ repopick.sh
@@ -2,4 +2,10 @@
 
 source build/envsetup.sh
 
-repopick -t pixel fingerprint-zl1_zl0
+repopick -t pixel
+
+repo sync --force-sync --no-tags --no-clone-bundle
+
+repopick 12209 12208
+
+repopick -t adaptive-playback configs fingerprint things battery-redo && repopick 12216
